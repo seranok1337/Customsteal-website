d768ceac204df1ce7c901285a117d9cf546eee50
seranok1337 <admin@zalupkaminecraft.xyz>
2024-12-21 00:49:59 +0200

    Main Commit
    Main Commit

---
+++ main.js
@@ -0,0 +1,26 @@
+const { app, BrowserWindow } = require('electron');
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    frame: false,
+    webPreferences: {
+      preload: __dirname + 'src/renderer.js'
+    }
+  });
+
+  mainWindow.loadFile('src/index.html');
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) createWindow();
+});
